36bd74d8c1fc1f3e649f55db91539d7bf237985c
AdiPrk <adityaprk@outlook.com>
2023-09-17 17:07:07 -0700

    Added premake for building project files

---
+++ premake5.lua
@@ -0,0 +1,107 @@
+workspace "Dog"
+	architecture "x64"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Dog"
+	location "Dog"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"%{prj.name}/vendor/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++20"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"DOG_PLATFORM_WINDOWS",
+			"DOG_BUILD_DLL"
+		}
+
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+		}
+
+	filter "configurations:Debug"
+		defines "DOG_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "DOG_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "DOG_DIST"
+		optimize "On"
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"Dog/vendor/spdlog/include",
+		"Dog/src"
+	}
+
+	links
+	{
+		"Dog" 
+	}
+
+	filter "system:windows"
+		cppdialect "C++20"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"DOG_PLATFORM_WINDOWS"
+		}
+
+	filter "configurations:Debug"
+		defines "DOG_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "DOG_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "DOG_DIST"
+		optimize "On"
